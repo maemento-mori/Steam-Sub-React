47fff34221e9ad5edc722b6747b0e72f981014cb
Maemento Mori <maementomori@gmail.com>
2023-08-19 12:01:10 -0700

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,57 @@
+// server/index.js
+
+const express = require('express')
+const app = express()
+
+const cheerio = require("cheerio")
+const axios = require("axios")
+
+// const port = 3000
+const PORT = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  fetch('https://steamcommunity.com/sharedfiles/filedetails/?id=2986581203')
+    .then(resp=> resp.text()).then(body => res.send(body)) ; 
+  
+})
+
+app.get('/testFetch', (req, res) => {
+
+  async function getProfileData() {
+    console.log("starting");
+    const axiosResponse = await axios.request({
+      method: "GET",
+      url: "https://steamcommunity.com/sharedfiles/filedetails/?id=2986581203",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+      },
+    });
+    const $ = cheerio.load(axiosResponse.data);
+
+    let subscribers = $(".stats_table").find("tr:nth-child(2)").find("td:first-child").text()
+
+    return subscribers
+  }
+
+  async function init() {
+    let userData = await getProfileData();
+    console.log("finished fetching data")
+
+    res.send({ message: userData })
+  }
+
+  init()
+  
+  
+})
+
+app.get('/test', (req, res) => {
+  res.send({ message: "Hello from server!" })
+  
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+
