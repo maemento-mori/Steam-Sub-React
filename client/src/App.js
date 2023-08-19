6bf6137429723ba948b9ed225cc453c3d20ad305
Maemento Mori <maementomori@gmail.com>
2023-08-19 12:09:34 -0700

    Added client folder (For React)

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+// client/src/App.js
+
+import React from "react";
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/test")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Subscribers.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Subscribers = () => {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    setInterval(() => {
+      fetch("/testFetch")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.message)
+        console.log(data.message)
+      });
+    },30000)
+  }, []);
+
+
+  
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <p>I am in a React Component!</p>
+      <p>{!data ? "Loading..." : data}</p>
+    </div>
+  );
+};
+export default Subscribers;
